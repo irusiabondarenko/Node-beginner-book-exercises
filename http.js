892291823e42c4e4c4169c97cfae647b610c6f5d
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-10-31 19:06:28 -0700

    Server retun diffeent responses to user

--- http.js
+++ http.js
@@ -6,10 +6,11 @@ function start (route, handle) {
 	function onRequest(request, response)  {
 		var pathname = url.parse(request.url).pathname;
 		console.log("request for " + pathname + "received");
-		route(handle, pathname);
+		
 
 		response.writeHead(200, {"Content-Type" : "text/plain"});
-		response.write("hello world");
+		var content = route(handle, pathname);
+		response.write(content);
 		response.end();
 	}
 
