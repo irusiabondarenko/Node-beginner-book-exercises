6ca5e5ffb831860e4aec84462b97b29867332b6f
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-10-31 18:47:39 -0700

    Created a few routes for http server

--- http.js
+++ http.js
@@ -2,11 +2,11 @@ var http = require("http");
 var url = require("url")
 
 
-function start (route) {
+function start (route, handle) {
 	function onRequest(request, response)  {
 		var pathname = url.parse(request.url).pathname;
 		console.log("request for " + pathname + "received");
-		route(pathname);
+		route(handle, pathname);
 
 		response.writeHead(200, {"Content-Type" : "text/plain"});
 		response.write("hello world");
@@ -16,5 +16,6 @@ function start (route) {
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started");
 }
+
 
 exports.start = start;
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 var server = require('./http');
 var router = require('./router');
+var requestHandlers = require("./handlers");
 
-server.start(router.route);
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
